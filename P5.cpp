3bd148d500c178daadf61d310d8865e0cdd93105
KrishnaNAcharya <knacharyakavoor@gmail.com>
2024-10-15 13:29:34 +0530

    peak

--- P5.cpp
+++ P5.cpp
@@ -6,19 +6,14 @@ compares the two numbers and returns the maximum. */
 #include<iostream> 
 using namespace std;
 
-template<typename T>
-class Max {
-    public:
+template<class T>
 T findmax(T a, T b){
     return (a>b)?a:b;
 }
-};
 
 int main(){
-    Max<int> m1;
-    cout<<m1.findmax(10,20)<<endl;
+    cout<<findmax(10,20)<<endl;
     
-    Max<double> m2;
-    cout<<m2.findmax(10.2,11.4)<<endl;
+    cout<<findmax(10.2,11.4)<<endl;
     return 0;
 }
